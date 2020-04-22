9fbd6e26e18de9cee4e5715b5af2d86812a66d6a
Dane Juras <verydanejuras@gmail.com>
2020-04-21 22:42:04 -0400

    correcting sort order of tags

--- AniDroid/AniListObject/Media/MediaPresenter.cs
+++ AniDroid/AniListObject/Media/MediaPresenter.cs
@@ -151,7 +151,7 @@ namespace AniDroid.AniListObject.Media
 
             if (media.Tags?.Any() == true)
             {
-                media.Tags = media.Tags.OrderBy(x => x.Rank).ToList();
+                media.Tags = media.Tags.OrderByDescending(x => x.Rank).ToList();
             }
         }
     }
